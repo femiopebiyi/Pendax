f80657e59c004c33a914a9d611eb0aef5322bcc3
opebiyi <opebiyibiodun10@gmail.com>
2025-08-11 21:12:21 -0700

    changed blurry logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,6 +2,7 @@ import pendaxIcon from  "../assets/icons/pendaxIcon.svg"
 import brighnessIcon from  "../assets/icons/brightnessIcon.svg"
 import bellIcon from  "../assets/icons/bellIcon.svg"
 import messageIcon from  "../assets/icons/messageIcon.svg"
+import pendaxLogo from "../assets/icons/pendaxLogo.png"
 
 
 export function Navbar(){
@@ -10,7 +11,7 @@ export function Navbar(){
 
     return <div className="navbar">
         <div className="con">
-            <img src={pendaxIcon} alt="pendaxIcon" />
+            <img src={pendaxIcon} alt="pendaxIcon" className="pendaxIcon"/>
             <button className="connnect">Connect Wallet</button>
         </div>
 
