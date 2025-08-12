2e5e511c20e1e6a38974de4288dca7ec62da88f2
opebiyi <opebiyibiodun10@gmail.com>
2025-08-11 21:16:38 -0700

    made some changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,6 @@ import pendaxIcon from  "../assets/icons/pendaxIcon.svg"
 import brighnessIcon from  "../assets/icons/brightnessIcon.svg"
 import bellIcon from  "../assets/icons/bellIcon.svg"
 import messageIcon from  "../assets/icons/messageIcon.svg"
-import pendaxLogo from "../assets/icons/pendaxLogo.png"
 
 
 export function Navbar(){
