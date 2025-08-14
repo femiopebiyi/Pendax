bff5c5fe148e9500e9314c9f1205a8d3a0ab5c1e
opebiyi <opebiyibiodun10@gmail.com>
2025-08-14 21:52:48 +0100

    made some design changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,13 +12,15 @@ export function Navbar(){
     return <div className="navbar">
         <div className="con">
             <img src={pendaxIcon} alt="pendaxIcon" className="pendaxIcon"/>
-            <button className="connnect">Connect Wallet</button>
+            <h2>Pendax</h2>
         </div>
 
-        <div className="smallIcons">
+        {/* <div className="smallIcons">
             <img src={brighnessIcon} alt="" />
             <img src={messageIcon} alt="" />
             <img src={bellIcon} alt="" />
-        </div>
+        </div> */}
+
+        <button className="connnect">Connect Wallet</button>
     </div>
 }
--- src/components/WalletBalance.tsx
+++ src/components/WalletBalance.tsx
@@ -1,17 +1,24 @@
 import eye_slash from "../assets/icons/eyeslash.svg"
 import arrowRight from "../assets/icons/arrow_right.svg"
+import { useState } from "react"
 
 export function WalletBalance(){
+
+    let [balanceVisibility, setBalanceVisibility] = useState(true)
+
+    function changeBalVisibility(){
+        setBalanceVisibility(!balanceVisibility)
+    }
 
 
     return <div className="walletBal">
             <div className="balance_sec">
-                <p>Wallet Balance &nbsp;&nbsp;<img src={eye_slash} alt="" /></p>
-                <p>****</p>
+                <p>Wallet Balance &nbsp;&nbsp;<img src={eye_slash} alt="" onClick={changeBalVisibility}/></p>
+                <p>{balanceVisibility ? "$1287.45" : "****"}</p>
             </div>
             <div className="history">
                 <p>Transaction History &nbsp;&nbsp;<img src={arrowRight} alt="" /></p>
-                <button className="pay">Send</button>
+                <button className="pay">Pay Bills</button>
             </div>
     </div>
 }
