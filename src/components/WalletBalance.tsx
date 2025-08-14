76f0cfe9389d3ea5a03e8be0796d20702f85c669
opebiyi <opebiyibiodun10@gmail.com>
2025-08-14 21:53:20 +0100

    made some design changes

--- src/components/WalletBalance.tsx
+++ src/components/WalletBalance.tsx
@@ -1,6 +1,7 @@
 import eye_slash from "../assets/icons/eyeslash.svg"
 import arrowRight from "../assets/icons/arrow_right.svg"
 import { useState } from "react"
+import eye_open from "../assets/icons/eyeIcon.svg"
 
 export function WalletBalance(){
 
@@ -13,7 +14,14 @@ export function WalletBalance(){
 
     return <div className="walletBal">
             <div className="balance_sec">
-                <p>Wallet Balance &nbsp;&nbsp;<img src={eye_slash} alt="" onClick={changeBalVisibility}/></p>
+                <p>
+  Wallet Balance &nbsp;&nbsp;
+  {balanceVisibility ? (
+    <img src={eye_open} alt="Hide Balance" onClick={changeBalVisibility} />
+  ) : (
+    <img src={eye_slash} alt="Show Balance" onClick={changeBalVisibility} />
+  )}
+</p>
                 <p>{balanceVisibility ? "$1287.45" : "****"}</p>
             </div>
             <div className="history">
